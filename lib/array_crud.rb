c68f7502c98a387c40ff5d3500331203517bd402
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-18 17:57:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- animals = ["cat", "dog", "horse", "mouse"]
-  animals << "bunny"
+ days = ["Monday", "Tuesday", "Wednesday"]
+  days << "Thursday"
   
 end
 
