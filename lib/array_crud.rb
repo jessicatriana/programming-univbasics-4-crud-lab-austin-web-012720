e3462226a6536b2f998dbbd7ea5ae9fc48ca2ead
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-18 17:56:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "bunny"
+ animals = ["cat", "dog", "horse", "mouse"]
+  animals << "bunny"
   
 end
 
